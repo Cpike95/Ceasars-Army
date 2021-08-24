98373e24d492f55e072d073e51657950bef3fa8f
Curtis Pike <58271540+Cpike95@users.noreply.github.com>
2021-08-23 18:13:53 -0700

    some styling and prettier

---
+++ ceasrs-army/.prettier.js
@@ -0,0 +1,14 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'always',
+    htmlWhitespaceSensitivity: 'css',
+  };

2ab2b2295a940ac97abafce0cb0679979d4efaeb
Curtis Pike <58271540+Cpike95@users.noreply.github.com>
2021-08-25 20:57:16 -0700

    Package.json spdate

--- client/src/components/About/about.js
+++ client/src/components/About/about.js
@@ -4,15 +4,19 @@ import './about.css'
 
 const About = () => {
 
+
+
     return (
         <div className='about-wrapper'>
             <div className='about-content'>
-                <div className=' box about-card ' data-san="fade" data-san-delay="400">
+            <div data-aos="animation_name">
+                <div className='about-card'>
                     About CARD 
                 </div>
                 <div className='about-img'>
                     <img src='' />
                 </div>
+            </div>
             </div>
         </div>
     )

d7b6e9c0ea231c558eb2851a45fd43808c2bdfb7
Curtis Pike <58271540+Cpike95@users.noreply.github.com>
2021-08-25 18:34:55 -0700

    Fixing packages

--- client/src/components/About/about.js
+++ client/src/components/About/about.js
@@ -7,7 +7,7 @@ const About = () => {
     return (
         <div className='about-wrapper'>
             <div className='about-content'>
-                <div className='about-card'>
+                <div className=' box about-card ' data-san="fade" data-san-delay="400">
                     About CARD 
                 </div>
                 <div className='about-img'>
